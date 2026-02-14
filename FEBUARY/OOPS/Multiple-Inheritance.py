dfad2a7035c30a7eb3485e47bc81200e33efed26
user <user@example.com>
2026-02-14 13:48:01 +0530

    Multiple Inheritance prac done

---
+++ FEBUARY/OOPS/Multiple-Inheritance.py
@@ -0,0 +1,9 @@
+class father:
+    def skills(self):
+        print("Coding")
+class mother:
+    def skills(self):
+        print("Cooking")
+class child(father,mother):
+    def child(self):
+        print("I have both skills")
