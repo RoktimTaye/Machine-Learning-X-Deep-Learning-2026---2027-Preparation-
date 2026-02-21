cdb035798c57bc88efdb71b2c72f64eacd68055e
user <user@example.com>
2026-02-21 14:19:46 +0530

    Prac done again with more clarity

--- FEBUARY/PracticeProject/MainPrac.py
+++ FEBUARY/PracticeProject/MainPrac.py
@@ -1,34 +1,20 @@
-import random
+import random 
 import json
 import string
 from pathlib import Path
+
 class Bank:
-    database = 'data,json'
-    data = []
-    @classmethod
-    def __update(cls):
-        with open(cls.database,'w') as fs:
-            fs.write(json.dumps(Bank.data))
-
-    @classmethod
-    def __accountgenerate(cls):
-        aplha = random.choices(string.ascii_letters,k=3)
-        num = random.choices(string.digits,k=3)
-        spchar = random.choices("@#$%^&*",k = 1)
-        id = aplha + num + spchar
-        random.shuffle(id)
-        return "".join(id)
-
     def createaccount():
         info = {
-            "name" : input("Tell your Name :- "),
-            "age" : int(input("Tell your age :- ")),
-            "email" : input("Tell your email :-"),
-            "pin" : int(input("Tell your 4 digit pin :- ")),
-            "accountNo" : Bank.__accountgenerate(),
-            "balance" : 0
+            'name' : input('Tell your name :- '),
+            'age' : int(input('Tell your age :- ')),
+            'email' : input('Tell your email :- '),
+            'pin' : int(input('Tell your 4 digit pin number :- ')),
+            'accountNo' : 1234,
+            'balance' : 0
         }
-user = Bank()
+# user = Bank()
+
 print("press 1 for creating an account")
 print("press 2 for Deposititing the money in the bank ")
 print("press 3 for withdrawing the money ")
