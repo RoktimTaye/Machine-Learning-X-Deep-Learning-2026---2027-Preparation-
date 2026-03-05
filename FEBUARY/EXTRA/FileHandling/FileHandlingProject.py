5c9ee805246f4bf6cbc513fd3642b6c9b4aa05bc
user <user@example.com>
2026-03-05 12:15:50 +0530

    Entered after a long break for Versity March Past Practice

--- FEBUARY/EXTRA/FileHandling/FileHandlingProject.py
+++ FEBUARY/EXTRA/FileHandling/FileHandlingProject.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 def readfilefolder():
-    path = path('')
+    path = Path('')
     items = list(path.rglob('*'))
     for i , items in enumerate(items):
         print(f"{i+1} : {items}")
